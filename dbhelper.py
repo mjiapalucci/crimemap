f3084503cfd758278de9a3b6b3229d60c31b4589
mjiapalucci <mj_iapalucci@yahoo.com>
2017-05-15 11:36:12 -0400

    connection error

--- dbhelper.py
+++ dbhelper.py
@@ -43,12 +43,12 @@ class DBHelper:
 	def add_crime(self, category, date, latitude, longitude, description):
 		connection = self.connect()
 		try:
-			query = "INSERT INTO crimes (category, date, latitude, longitude, description) VALUES (%s, %s, %s, %s, %s);"
+			query = "INSERT INTO crimes (category, date, latitude, longitude, description) VALUES (%s, %s, %s, %s, %s)"
 			with connection.cursor() as cursor:
 				cursor.execute(query, (category, date, latitude, longitude, description))
-				connect.commit()
+				connection.commit()
 		except Exception as e:
 			print(e)
 		finally:
-			connect.close()
+			connection.close()
 
